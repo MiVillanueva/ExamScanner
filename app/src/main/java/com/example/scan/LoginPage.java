4af84c45e9e34de65dd2feb0bc0032497ae4f424
MiVillanueva <100418128+MiVillanueva@users.noreply.github.com>
2024-06-02 00:08:22 +0800

    Deleted MainPage.java and activity_main

--- app/src/main/java/com/example/scan/LoginPage.java
+++ app/src/main/java/com/example/scan/LoginPage.java
@@ -51,7 +51,7 @@ public class LoginPage extends AppCompatActivity {
                 String password = passwordInput.getText().toString().trim();
 
                 if(isValidCredentials(username, password)){
-                    Intent intent = new Intent(LoginPage.this, MainPage.class);
+                    Intent intent = new Intent(LoginPage.this, Scanner.class);
                     startActivity(intent);
                 } else {
                     Toast.makeText(LoginPage.this, "Invalid credentials. Please enter valid email and password.", Toast.LENGTH_SHORT).show();
