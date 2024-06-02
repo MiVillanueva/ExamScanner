d42a43d5b7face4dfb9b8e4487a70bc4203bfa85
MiVillanueva <100418128+MiVillanueva@users.noreply.github.com>
2024-06-02 13:49:27 +0800

    Removed LandingPage java and xml. Modified LoginPage

--- app/src/main/java/com/example/scan/LoginPage.java
+++ app/src/main/java/com/example/scan/LoginPage.java
@@ -51,7 +51,7 @@ public class LoginPage extends AppCompatActivity {
                 String password = passwordInput.getText().toString().trim();
 
                 if(isValidCredentials(username, password)){
-                    Intent intent = new Intent(LoginPage.this, activity_LandingPage.class);
+                    Intent intent = new Intent(LoginPage.this, activity_main.class);
                     startActivity(intent);
                 } else {
                     Toast.makeText(LoginPage.this, "Invalid credentials. Please enter valid email and password.", Toast.LENGTH_LONG).show();
