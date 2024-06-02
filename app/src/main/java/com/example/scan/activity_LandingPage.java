64d9a023737c40e87ea4b83f1272e8ad4e9eb99e
dyzrl <dyzrlynvrgs@gmail.com>
2024-06-03 00:50:03 +0800

    navbar

--- app/src/main/java/com/example/scan/activity_LandingPage.java
+++ app/src/main/java/com/example/scan/activity_LandingPage.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.scan.R;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_LandingPage extends AppCompatActivity {
 
@@ -18,10 +17,10 @@ public class activity_LandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
-        Button scan = findViewById(R.id.button3);
+        Button setting = findViewById(R.id.button3);
         Button add = findViewById(R.id.button2);
 
-        scan.setOnClickListener(new View.OnClickListener() {
+        setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Define the action for scanner button
@@ -38,6 +37,26 @@ public class activity_LandingPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_notifications:
+                    // Handle Notification item selection
+                    return true;
+                case R.id.nav_home:
+                    // Handle home item selection
+                    return true;
+                case R.id.nav_scanner:
+                    // Handle scanner item selection
+                    return true;
+                case R.id.nav_profile:
+                    //Handle profile item selection
+                    return true;
+            }
+            return false;
+        });
+
 
     }
 }
